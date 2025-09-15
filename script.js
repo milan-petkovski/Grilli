fa70748a84dab83dd9c05287d469539e8aedf306
Milan Petkovski <milan.petkovski1969@gmail.com>
2025-09-15 23:27:20 +0200

    Popup, header fix

--- script.js
+++ script.js
@@ -33,12 +33,18 @@ const navTogglers = document.querySelectorAll("[data-nav-toggler]");
 const overlay = document.querySelector("[data-overlay]");
 
 const toggleNavbar = function () {
-  navbar.classList.toggle("active");
-  overlay.classList.toggle("active");
-  document.body.classList.toggle("nav-active");
+  if (window.innerWidth < 1200) {
+    navbar.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+  }
 }
 
 addEventOnElements(navTogglers, "click", toggleNavbar);
+
+const navbarLinks = document.querySelectorAll(".navbar-link");
+addEventOnElements(navbarLinks, "click", function () {
+});
 
 //#endregion
 
@@ -47,24 +53,10 @@ addEventOnElements(navTogglers, "click", toggleNavbar);
 const header = document.querySelector("[data-header]");
 const backTopBtn = document.querySelector("[btt]");
 
-let lastScrollPos = 0;
-
-const hideHeader = function () {
-  const isScrollBottom = lastScrollPos < window.scrollY;
-  if (isScrollBottom) {
-    header.classList.add("hide");
-  } else {
-    header.classList.remove("hide");
-  }
-
-  lastScrollPos = window.scrollY;
-}
-
 window.addEventListener("scroll", function () {
   if (window.scrollY >= 50) {
     header.classList.add("active");
     backTopBtn.classList.add("active");
-    hideHeader();
   } else {
     header.classList.remove("active");
     backTopBtn.classList.remove("active");
