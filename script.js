ea570a949e53de54879e34ec1ebc09f2aa33a8e6
Milan Petkovski <milan.petkovski1969@gmail.com>
2025-10-16 17:41:43 +0200

    Remove notification on enter website

--- script.js
+++ script.js
@@ -7,17 +7,6 @@ window.addEventListener("load", function () {
   document.body.classList.add("loaded");
 });
 
-
-function showPopup() {
-  document.getElementById('popup').style.display = 'block';
-  document.body.classList.add('noscroll');
-}
-
-function closePopup() {
-  document.getElementById('popup').style.display = 'none';
-  document.body.classList.remove('noscroll');
-}
-
 const addEventOnElements = function (elements, eventType, callback) {
   for (let i = 0, len = elements.length; i < len; i++) {
     elements[i].addEventListener(eventType, callback);
